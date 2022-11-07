ac6c6679941a1b4450283e6558677ead904b0a64
cahein <incidents1371@gmail.com>
2022-11-07 12:21:43 +0900

    20221107

--- 011.py
+++ 011.py
@@ -3,50 +3,28 @@ import io
 import sys
 
 _INPUT = """\
-
+10
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-S = "13 DUP 4 POP 5 DUP + DUP + -"
+N = int(input())
 
-def solution(expression):
-    stack = []
-    for i in expression.split(' '):
-        if i == '+' and len(stack)>=2:
-            # +のときはスタックから2つ取り出して加算し，再度格納する
-            b, a = stack.pop(), stack.pop() #後の演算のために順序を整えている
-            stack.append(a + b)
+import math
 
-        elif i == '-' and len(stack)>=2:
-            # -のときはスタックから2つ取り出して減算し，再度格納する
-            b, a = stack.pop(), stack.pop() #後の演算のために順序を整えている
-            stack.append(b - a)
-
-        elif i == 'DUP':
-            # -のときはスタックから2つ取り出して減算し，再度格納する
-            a = stack.pop() #後の演算のために順序を整えている
-            stack.append(a)
-            stack.append(a)
-        elif i == 'POP':
-            # -のときはスタックから2つ取り出して減算し，再度格納する
-            a = stack.pop() #後の演算のために順序を整えている
-            #stack.append(a)
-        elif i == '+' and len(stack)<2:
-            return -1
-        elif i == '-' and len(stack)<2:
-            return -1
-        else:
-            stack.append(int(i))    #受け取っているものは文字列であるが，後に計算されるので，intに変換
-
-    temp = stack.pop()
-    if temp<0:
-        temp=-1
-    elif temp>2**20-1:
-        temp =-1
-
-    return temp
+def check(n):
+    ans = []
+    for i in range(2,n+1):
+        flag = True
+        for j in range(2,i+1):
+            print (i,j)
+            if i%j!=0:
+                flag = False
+        if flag ==True:
+            ans.append(i)
+    return ans
 
 
+count = 0
 
-print (solution(S))
+print (check(N))
