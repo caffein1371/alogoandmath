44b70e01b22ebd65593f0a53d202db9634ff86cf
cahein <incidents1371@gmail.com>
2022-11-23 21:18:33 +0900

    20221123

--- 054.py
+++ 054.py
@@ -11,11 +11,12 @@ sys.stdin = io.StringIO(_INPUT)
 from functools import lru_cache
 
 @lru_cache(maxsize=None)
-def fib(N):
-    if N ==1 or N==2:
+def fib(n):
+    if n ==1 or n==2:
         return 1
-    else:
-        return fib(N-1)+fib(N-2)
+    
+    return fib(n-1)+fib(n-2)
+
 N = int(input())
 print (fib(N)%10**9)
 
