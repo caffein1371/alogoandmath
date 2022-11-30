eb9ec648061dbb14d451280135280017ca4cb8e2
cahein <incidents1371@gmail.com>
2022-11-30 22:34:08 +0900

    20221130

--- 030.py
+++ 030.py
@@ -3,8 +3,13 @@ import io
 import sys
 
 _INPUT = """\
-1 10
-9 1
+6 15
+6 5
+5 6
+6 4
+6 6
+3 5
+7 2
 
 
 
@@ -12,16 +17,18 @@ _INPUT = """\
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
 N,W = map(int,input().split())
-w= [0]
-v= [0]
-for i in range(N):
+w= [0]*(N+1)
+v= [0]*(N+1)
+for i in range(1,N+1):
     a,b = map(int,input().split())
-    w.append(a)
-    v.append(b)
-dp = [[0 for i in range(110)] for j in range(100000)]
-dp[0][0] = 0
+    w[i]=a
+    v[i]=b
+#1つ目の配列が2つ目
+dp = [[0 for i in range(W+1)] for j in range(N+1)]
+
 for i in range(1,W+1):
-    dp[0][i] = -10**9
+    dp[0][i] = -1
+
 for i in range(1,N+1):
     for j in range(W+1):
         #j<w[i]の時、次の商品を選べない
